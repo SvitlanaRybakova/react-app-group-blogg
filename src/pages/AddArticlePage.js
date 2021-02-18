9447ac5fc6a652d0b23af532ca4ef0f4bb6cad38
Elena <sagva2014@gmail.com>
2021-02-18 13:09:02 +0100

    Added Form for adding articles

--- src/pages/AddArticlePage.js
+++ src/pages/AddArticlePage.js
@@ -1,8 +1,26 @@
+import { useContext, useState } from "react";
+import { ArticleContext } from '../contexts/ArticleContext'
+
 function AddArticlePage() {
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+    const [date, setDate] = useState('')
+    const [author, setAuthor] = useState('')
+
+    const article = {
+        title,
+        content,
+        date: '18 feb 2020',
+        author
+    }
+
     return(
-        <div>
-            Here will be form for adding articles
-        </div>
+        <form>
+            <input type="text" placeholder="title" onChange={(e) => setTitle(e.target.value)}/>
+            <input type="text" placeholder="content" onChange={(e) => setContent(e.target.value)}/>
+            <input type="text" placeholder="author" onChange={(e) => setAuthor(e.target.value)}/>
+            <button type='submit'>Submit</button>
+        </form>
     )
 }
 
