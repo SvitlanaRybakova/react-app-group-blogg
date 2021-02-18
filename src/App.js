e918ec19f6977858d54fae530d190694ae40d80c
Oliver Kellgren <oliver-fotboll@live.se>
2021-02-18 11:38:50 +0100

    removed one home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { BrowserRouter, Route } from "react-router-dom";
-import Home from './pages/Home'
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
 
 function App() {
   return (
