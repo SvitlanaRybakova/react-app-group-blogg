48b13c0d2a50e7648ba3132f58eceef683daccd7
Svitlana <rsv0011@gmail.com>
2021-02-19 18:39:20 +0100

    implemented toggleTheme to About page

--- src/components/ArticleItem.js
+++ src/components/ArticleItem.js
@@ -1,27 +1,30 @@
 import styles from "../styles/ArticleItem.module.css";
 import { ArticleContext } from "../contexts/ArticleContext";
 import { useContext } from "react";
-import {ThemeContext} from "../contexts/ThemeContext";
+import { ThemeContext } from "../contexts/ThemeContext";
 
-function ArticleItem(){
+function ArticleItem() {
   const { articles } = useContext(ArticleContext);
   const { isGreenTheme, green, dark } = useContext(ThemeContext);
   const theme = isGreenTheme ? green : dark;
 
-  return(
+  return (
     <div className={styles.articleItem}>
-      {articles.map(article =>{
-        return(
-          <section className={styles.content} 
-          key={article.id} 
-          style= {{background: theme.bg, color: theme.fontColor}}>
-          <h1 className={styles.header}>{article.title}</h1>
-          <p className={styles.article}>{article.content}</p>
-          <div className={styles.detailContainer}>
-            <span className={styles.timestamp}>{article.date}</span>
-            <span className={styles.author}>{article.author}</span>
-          </div>
-      </section>
+      {articles.map(article => {
+        return (
+
+          <section
+            className={styles.content}
+            key={article.id}
+            style={{ background: theme.bg, color: theme.fontColor }}
+          >
+            <h1 className={styles.header}>{article.title}</h1>
+            <p className={styles.article}>{article.content}</p>
+            <div className={styles.detailContainer}>
+              <span className={styles.timestamp}>{article.date}</span>
+              <span className={styles.author}>{article.author}</span>
+            </div>
+          </section>
         )
       })}
     </div>
